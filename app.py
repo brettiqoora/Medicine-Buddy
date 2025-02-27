f94a3968b380491213d4c2ace903b737993768b0
brettdelport <122012321+brettdelport@users.noreply.github.com>
2025-02-27 22:03:50 +0200

    First Commit

--- app.py
+++ app.py
@@ -8,143 +8,73 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-    owner = db.Column(db.String(200), nullable=False)
-    date_completed = db.Column(db.DateTime, nullable=True)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+class Drug(db.Model):
+    code = db.Column(db.String(20), primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    atc_code = db.Column(db.String(20), nullable=False)
+    dosage_form = db.Column(db.String(20), nullable=True)
+    pack_size = db.Column(db.Integer, nullable=True)
+    pack_price = db.Column(db.Float, nullable=True)
 
     def __repr__(self):
         return '<Task %r>' % self.id
 
-class Owners(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(200), nullable=False)
-    
-    def __repr__(self):
-        return '<Owner %r>' % self.id
-    
 with app.app_context():
         db.create_all()
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/', methods=['GET','POST'])
 def index():
-    if request.method == 'POST':
-         task_content = request.form['content']
-         task_owner = request.form['owner']
-         new_task = Todo(content=task_content, owner=task_owner, date_completed = None)
+  if request.method == 'GET':
+    return render_template('index.html')
 
-         try:
-             db.session.add(new_task)
-             db.session.commit()
-             return redirect('/')
-         except:
-             return 'There was an issue adding the task ' + task_content + ' '  +  task_owner                                                                                                                                                                      
+@app.route('/find_by_code', methods=['POST'])
+def find_by_code():
+    if request.method == 'POST':
+      in_code = request.form['code']
+      if in_code.strip() != "":
+
+        found_drugs = Drug.query.filter(Drug.name.ilike(f"{in_code}%")).order_by((Drug.pack_price / Drug.pack_size).asc()).all()
+        if found_drugs is None:
+          return f'There is no drug found matching {in_code}'
+        else:
+          return render_template('choosedrug.html',drugs = found_drugs)
+      else:
+         return f'No code entered'
+      
+@app.route('/home', methods=['GET'])
+def home():
+    if request.method == 'GET':
+      return render_template('index.html')   
+
+@app.route('/find_by_name', methods=['POST'])
+def find_by_name():
+    if request.method == 'POST':
+      in_name = request.form['name']
+      if in_name.strip() != "":
+
+        found_drugs = Drug.query.filter(Drug.name.ilike(f"{in_name}%")).order_by((Drug.pack_price / Drug.pack_size).asc()).all()
+        if found_drugs is None:
+          return f'There is no drug found matching {in_name}'
+        else:
+          return render_template('choosedrug.html',drugs = found_drugs)
+      else:
+         return f'No name entered'
+
+@app.route('/fetch/<string:drug_dosage_form>/<string:drug_atc_code>', methods=['GET'])
+def fetch(drug_dosage_form,drug_atc_code):
+    if request.method == 'GET':
+      found_drugs = Drug.query.filter(
+        Drug.dosage_form == drug_dosage_form,
+        Drug.atc_code == drug_atc_code).order_by((Drug.pack_price / Drug.pack_size).asc()).all()
+
+
+    if found_drugs is None:
+      return f'There is no equivilent drugs found'
     else:
-      tasks = Todo.query.order_by(Todo.date_created).all()
-      owners = Owners.query.order_by(Owners.id).all()
+      return render_template('listequivelents.html',drugs = found_drugs)
 
-      return render_template('index.html',tasks = tasks, owners = owners)
-    
-@app.route('/AddOwner', methods=['POST', 'GET'])
-def addOwner():
-    if request.method == 'POST':
-         task_content = request.form['content']
-         task_owner = request.form['owner']
-         new_task = Todo(content=task_content, owner=task_owner, date_completed = None)
-
-         try:
-             db.session.add(new_task)
-             db.session.commit()
-             return redirect('/')
-         except:
-             return 'There was an issue adding the task ' + task_content + ' '  +  task_owner                                                                                                                                                                      
-    else:
-      tasks = Todo.query.order_by(Todo.date_created).all()
-      owners = Owners.query.order_by(Owners.id).all()
-
-      return render_template('index.html',tasks = tasks, owners = owners)    
-    
-@app.route('/delete/<int:id>')
-def delete(id):
-    task_to_delete = Todo.query.get_or_404(id)
-
-    try:
-        db.session.delete(task_to_delete)
-        db.session.commit()
-        return redirect('/')
-    except:
-        return 'There was an issue deleting the task'
-    
-@app.route('/ownerDelete/<int:id>')
-def ownerDelete(id):
-    owner_to_delete = Owners.query.get_or_404(id)
-
-    try:
-        db.session.delete(owner_to_delete)
-        db.session.commit()
-        return redirect('/gotoAddOwner')
-    except:
-        return 'There was an issue deleting the task'    
-    
-@app.route('/complete/<int:id>')
-def complete(id):
-    task_to_complete = Todo.query.get_or_404(id)
-    if task_to_complete.date_completed is None:
-      task_to_complete.date_completed = datetime.now(timezone.utc)
-    else: 
-      task_to_complete.date_completed = None  
-    try:
-      db.session.commit()
-      return redirect('/')
-    except:
-      return 'There was an issue completing the task'
-    
-@app.route('/update/<int:id>', methods=['POST', 'GET'])
-def update(id):
-    
-    task_to_update = Todo.query.get_or_404(id)
-    owners = Owners.query.order_by(Owners.id).all()
-
-    if request.method == 'POST':
-      updated_content = request.form['content']
-      updated_owner = request.form['owner']
-
-      task_to_update.content = updated_content
-      task_to_update.owner = updated_owner
-
-      try:
-        db.session.commit()
-        return redirect('/')
-      except:
-        return 'There was an issue updating the task'
-
-    else: 
-      return render_template('update.html',task = task_to_update, owners = owners)
-
-@app.route('/gotoAddOwner/', methods=['POST', 'GET'])
-def gotoAddOwner():
-  if request.method == 'POST':
-       ownerName = request.form['ownerName']
-       ownerID = request.form['Ownerid']
-         
-       new_owner = Owners(id=ownerID, name=ownerName)
-
-       try:
-         db.session.add(new_owner)
-         db.session.commit()
-         return redirect('/gotoAddOwner')
-       except:
-         return 'There was an issue adding this Owner ' + ownerID + ' '  +  ownerName
-  else:
-      MaxOwnerID = db.session.query(func.max(Owners.id+1)).scalar()
-      owners = Owners.query.order_by(Owners.id).all()
-      return render_template('addOwner.html',NewOwnerID = MaxOwnerID, owners = owners)  
-       
-    
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True, ssl_context='adhoc')
-    ##)
+
+    app.run()
     ##app.run(debug=True)
 
